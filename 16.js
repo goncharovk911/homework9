e5a05125787d472b89575c750ba335de2c38cc8d
Kostya <goncharovk911@gmail.com>
2020-01-08 17:19:25 +0200

    PushInput

---
+++ 16.js
@@ -0,0 +1,12 @@
+
+let but = document.createElement('button')
+but.type = 'button'
+but.innerHTML = 'Push'
+document.body.appendChild(but)
+but.onclick = () =>{
+    let inp = document.createElement('input')
+inp.type = 'input'
+inp.id = 'textColor'
+document.body.appendChild(inp)
+let but = document.createElement('button')
+} 
